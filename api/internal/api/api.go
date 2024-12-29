810ad94f20341874b131f558dbf8284bc549608f
Emil Andersson <eander0105@gmail.com>
2024-12-29 16:39:14 +0100

    nginx local dev env

--- api/internal/api/api.go
+++ api/internal/api/api.go
@@ -19,15 +19,6 @@ func Init() {
 	router := mux.NewRouter()
 	router.Use(LogMW)
 
-	// router.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-	// 	resp := Healthcheck{Status: true}
-	// 	json.NewEncoder(w).Encode(resp)
-	// })
-	// router.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-	// 	resp := Response{Message: "Hello, World!!!"}
-	// 	json.NewEncoder(w).Encode(resp)
-	// }).Methods("GET")
-
 	router = setUpRoutes(router)
 
 	log.Fatal(http.ListenAndServe(":3000", router))
