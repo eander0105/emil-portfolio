0335caaeb18d82f78a7250c0c4e02d8f3f2552c0
Emil Andersson <eander0105@gmail.com>
2025-03-22 23:02:59 +0100

    mux -> gin!

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,13 +1,6 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"encoding/json"
-
-	"github.com/gorilla/mux"
-)
+import "github.com/gin-gonic/gin"
 
 type Healthcheck struct {
 	Status bool `json:"status"`
@@ -18,20 +11,11 @@ type Response struct {
 }
 
 func main() {
-	router := mux.NewRouter()
-	// router.Use(LogMW)
+	router := gin.Default()
 
-	router = setUpRoutes(router)
-
-	log.Fatal(http.ListenAndServe(":3000", router))
-}
-
-func setUpRoutes(r *mux.Router) *mux.Router {
-	// testroutes
-	r.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-		resp := Healthcheck{Status: true}
-		json.NewEncoder(w).Encode(resp)
+	// Healthcheck route
+	router.GET("/healthcheck", func(c *gin.Context) {
+		c.JSON(200, Healthcheck{Status: true})
 	})
 
-	return r
 }
