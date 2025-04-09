b218780304b1d52c882d903e27480f0d9ffbe201
Emil Andersson <eander0105@gmail.com>
2025-04-09 23:24:04 +0200

    crud for project table

---
+++ api/internal/crud/project.go
@@ -0,0 +1,29 @@
+package crud
+
+import (
+	"github.com/eander0105/emil-portfolio/api/internal/db"
+	"github.com/eander0105/emil-portfolio/api/internal/models"
+)
+
+func GetAllProjects() ([]models.Project, error) {
+	var projects []models.Project
+	if err := db.DB.Find(&projects).Error; err != nil {
+		return nil, err
+	}
+	return projects, nil
+}
+
+func GetProjectByID(id uint) (*models.Project, error) {
+	var project models.Project
+	if err := db.DB.First(&project, id).Error; err != nil {
+		return nil, err
+	}
+	return &project, nil
+}
+
+func CreateProject(project *models.Project) error {
+	if err := db.DB.Create(project).Error; err != nil {
+		return err
+	}
+	return nil
+}
