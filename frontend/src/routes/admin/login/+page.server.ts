81ab061b76758487c87dd47134dc0e349fda032b
Emil Andersson <eander0105@gmail.com>
2025-04-19 16:08:26 +0200

    login form, not functional

---
+++ frontend/src/routes/admin/login/+page.server.ts
@@ -0,0 +1,17 @@
+import type { Actions, PageServerLoad } from './$types'
+import { superValidate } from 'sveltekit-superforms'
+import { loginSchema } from './schema'
+import { zod } from 'sveltekit-superforms/adapters'
+
+export const load: PageServerLoad = async () => {
+	return {
+		form: await superValidate(zod(loginSchema))
+	}
+};
+
+export const actions: Actions = {
+	default: async (event) => {
+		// Handle login
+		
+	}
+}
--- frontend/src/routes/admin/login/+page.svelte
+++ frontend/src/routes/admin/login/+page.svelte
@@ -1,1 +1,71 @@
-Hejsan?
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import * as Form from '$lib/components/ui/form'
+	import { Input } from '$lib/components/ui/input';
+
+	import {
+		type Infer,
+		type SuperValidated,
+		superForm
+	} from 'sveltekit-superforms';
+	import {
+		type LoginSchema,
+		loginSchema
+	} from './schema'
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	let { data }: { data: { form: SuperValidated<Infer<LoginSchema>> } } = $props();
+
+	const form = superForm(data.form, {
+		validators: zodClient(loginSchema),
+	})
+
+	const { form: formData, enhance } = form;
+</script>
+
+<div class="h-dvh">
+	<Card.Root class="my-8 mx-4 md:mx-auto md:w-[400px]">
+		<Card.Header>
+			<Card.Title class="text-center">Admin login</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<form use:enhance>
+				<Form.Field {form} name="email">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Form.Label>Email</Form.Label>
+							<Input type="email" {...props} bind:value={$formData.email} />
+						{/snippet}
+					</Form.Control>
+				</Form.Field>
+				<Form.Field {form} name="password">
+					<Form.Control>
+						{#snippet children({ props })}
+							<Form.Label>Password</Form.Label>
+							<Input type="password" {...props} bind:value={$formData.password} />
+						{/snippet}
+					</Form.Control>
+				</Form.Field>
+				<!-- <Form.Field {form} name="password">
+					<Form.Control>
+						<Form.Label>Password</Form.Label>
+					</Form.Control>
+				</Form.Field> -->
+				<!-- <div class="grid gap-4">
+					<div class="flex flex-col gap-2">
+						<Label for="email">Email</Label>
+						<Input id="email" type="email" />
+					</div>
+					<div class="flex flex-col gap-2">
+						<Label for="password">Password</Label>
+						<Input id="password" type="password" />
+					</div>
+				</div> -->
+			</form>
+		</Card.Content>
+		<Card.Footer>
+			<Button class="w-full">Login</Button>
+		</Card.Footer>
+	</Card.Root>
+</div>
