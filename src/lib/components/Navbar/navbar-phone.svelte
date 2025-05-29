ff6bcc5e6a57ea8c0693c97a617f16f933552106
Emil Andersson <eander0105@gmail.com>
2025-05-29 15:25:35 +0200

    cleanup

--- src/lib/components/Navbar/navbar-phone.svelte
+++ src/lib/components/Navbar/navbar-phone.svelte
@@ -54,37 +54,3 @@
 		</Sheet.Footer>
 	</Sheet.Content>
 </Sheet.Root>
-<!-- <div
-	class={'links ctransition-all absolute left-0 flex h-screen w-full duration-200 sm:hidden ' +
-		($burgerOpen
-			? 'visible translate-y-0 opacity-100 ease-in-out'
-			: 'invisible -translate-y-4 opacity-0')}
->
-	<div class="mt-20 flex w-full flex-col px-4">
-		{@render children?.()}
-	</div>
-
-	<div class="absolute bottom-6 w-full px-4 sm:hidden">
-
-		<Separator class="my-2"/>
-
-		<div class="flex w-full flex-row justify-around pb-4 pt-2">
-			<a
-				class="flex flex-row"
-				href="https://github.com/eander0105"
-				target="_blank"
-				title="GitHub"
-			>
-				<Icon icon="mdi:github" height="20" class="mr-2" /> Github
-			</a>
-			<a
-				class="flex flex-row"
-				href="https://www.linkedin.com/in/emil-andersson-846b22203/"
-				target="_blank"
-				title="LinkedIn"
-			>
-				<Icon icon="mdi:linkedin" height="20" class="mr-2" /> LinkedIn
-			</a>
-		</div>
-	</div>
-</div> -->
