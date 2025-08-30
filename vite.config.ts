7c5936842e84f3c60be4cb6b8ef41e9e738ed401
Emil Andersson <eander0105@gmail.com>
2025-08-30 21:38:35 +0200

    defineConfig as function, custom for prod/dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		host: '0.0.0.0',
-		port: 5173,
-		allowedHosts: ['.emilandersson.se'],
-	},
+export default defineConfig(({command}) => {
+	const production = command === 'build';
+	
+	// Allow *.emilandersson.se during development
+	const allowedHosts = production ? ['emilandersson.se'] : ['.emilandersson.se'];
+
+	return {
+		plugins: [sveltekit()],
+		server: {
+			host: '0.0.0.0',
+			port: 5173,
+			allowedHosts,
+		}
+	};
 });
