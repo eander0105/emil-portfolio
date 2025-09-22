fff34a1396138a641017d4c0f872a0ff288c1aaa
Emil Andersson <eander0105@gmail.com>
2025-09-22 23:59:02 +0200

    reordering page

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -6,18 +6,18 @@
 <br/>
 <p class="text-base">
 	or rather the raspberry pi that hosts this page you're currenly viewing
-	that happends to be in my home office :)<br>
-	<br>
+	that happends to be in my home office :)
+	<br><br>
 
-	So who am I and what do I do?<br>
-	<br>
-
-	I'm your regular IT facinated guy that really likes to accept and conquer challenge.
-	I might be a bit too curios fella since I seeminly can't stop to get into new intrests.<br>
-	<br>
+	So who am I and what do I do?
+	<br><br>
 
 	Professionally I'm currently working as a Fullstack developer at <Link href="zoezi.se">Zoezi</Link>
-	<br>
+	<br><br>
+
+	I'm your regular IT facinated guy that really likes to accept and conquer challenge.
+	I might be a bit too curios fella since I seeminly can't stop to get into new intrests.
+	<br><br>
 
 	<!-- On my freetime I enjoy tinkering with my own <Link href="/projects">projects</Link> -->
 </p>
